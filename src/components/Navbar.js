d45bac4d35140e85be117e53673a664b144ed9f3
kumarpanuganti <panugantikumar4@gmail.com>
2024-07-08 22:42:09 +0530

    home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,10 @@ const Navbar = () => {
       <h1 className="nav-container-hdng">KUMAR</h1>
       <div className={isOpen ? "nav-container-list active" :"nav-container-list"}>
         <h5>Home</h5>
-        <h5>Home</h5>
-        <h5>Home</h5>
-        <h5>Home</h5>
-        <h5>Home</h5>
+        <h5>About</h5>
+        <h5>My Work</h5>
+        <h5>Skills</h5>
+        <h5>Contact</h5>
       </div>
         <button className="nav-btn" onClick={toggleNavbar}>{isOpen ? <FaTimes /> : <FaBars />}</button>
     </nav>
