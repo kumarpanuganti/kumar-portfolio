5ca83e05ab88cb00deeddfc1ba08b6773d4830aa
kumarpanuganti <panugantikumar4@gmail.com>
2024-07-08 12:04:36 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav className="nav-container">
+      <h1 className="nav-container-hdng">K</h1>
+      <div className={isOpen ? "nav-container-list active" :"nav-container-list"}>
+        <h5>Home</h5>
+        <h5>Home</h5>
+        <h5>Home</h5>
+        <h5>Home</h5>
+        <h5>Home</h5>
+      </div>
+        <button className="nav-btn" onClick={toggleNavbar}>{isOpen ? <FaTimes /> : <FaBars />}</button>
+    </nav>
+  );
+};
+
+export default Navbar;
